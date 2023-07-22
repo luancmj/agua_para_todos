4faef6b6a454a396bd5976aee073de6096d80f5c
LCarlos94 <luan@userh.com.br>
2023-07-22 11:10:12 -0300

    Add flutter_config package and adjust main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:agua_para_todos/pages/source_page.dart';
+import 'package:agua_para_todos/repositories/source_repository.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterConfig.loadEnvVariables();
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
+  runApp(
+    ChangeNotifierProvider<SourcesRepository>(
+      create: (_) => SourcesRepository(),
+      child: SourceApp(),
+    ),
+  );
 }
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
+class SourceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+      title: 'Nascentes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
+      home: SourcePage(),
     );
   }
 }
